0319ec69a1b036074efca13ab54243591874bc28
Michael Hueschen <m@mhueschen.dev>
2024-12-24 10:26:16 -0800

    change training printout to decimal formal

--- chapter1_2/Network.py
+++ chapter1_2/Network.py
@@ -39,7 +39,7 @@ class Network(object):
 			for mini_batch in mini_batches:
 				self.update_mini_batch(mini_batch,eta)
 			if test_data:
-				print("Epoch {0}: {1} / {2}".format(j,self.evaluate(test_data),n_test))
+				print("Epoch {0}: {1:.4f}".format(j,(self.evaluate(test_data) / n_test)))
 			else:
 				print("Epoch {0} complete".format(j))
 			
