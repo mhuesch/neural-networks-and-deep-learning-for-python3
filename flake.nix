c65e4c5287dbbae79ae526965b441ac9e5e4f9ef
Michael Hueschen <m@mhueschen.dev>
2024-12-24 09:26:10 -0800

    get numpy to work
    painful on NixOS, as always.

--- flake.nix
+++ flake.nix
@@ -12,7 +12,18 @@
             let pkgs = import nixpkgs { inherit system; };
             in
             pkgs.mkShell {
+              shellHook = ''
+                export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
+                  pkgs.stdenv.cc.cc
+                  pkgs.zlib
+                ]}
+              '';
               buildInputs = with pkgs; [
+                # setup stuff
+                zlib
+                stdenv.cc.cc.lib
+
+                # python
                 (python311.withPackages(ps: []))
                 python311Packages.pip
               ];
